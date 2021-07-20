80ea01771477f8d372a7315f6cb51c31e899fc78
Liu Shuiyuan <13783534092@sina.cn>
2021-07-20 15:07:28 +0800

    add start

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -3,7 +3,8 @@ function component() {
     const element = document.createElement("div");
 
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-    element.innerHTML = _.join(["Hello", "webpack"], " ");
+	// lodash 在当前script中使用import引入
+	element.innerHTML = _.join(["Hello", "webpack"], " ");
 
     return element;
 }
