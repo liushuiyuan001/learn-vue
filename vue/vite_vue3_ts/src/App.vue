0f140468c348bb8a47dbf51788b9baf437a97155
Liu Shuiyuan <13783534092@sina.cn>
2022-01-30 14:26:05 +0800

    update:vue3-demo

---
+++ vue/vite_vue3_ts/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="test">Test {{mainStore.name}}</div>
+  <n-button>naive-ui</n-button>
+
+  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import { useMainStore } from "./store/main";
+import { NButton } from "naive-ui"
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,NButton
+  },
+  setup() {
+	  return { 
+		  mainStore: useMainStore()
+	  }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.test {
+	color: $test-color;
+}
+</style>
---
+++ vue/vite_vue3_ts/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createPinia } from 'pinia'
+import router from "./router/index"
+createApp(App).use(router).use(createPinia()).mount("#app");
