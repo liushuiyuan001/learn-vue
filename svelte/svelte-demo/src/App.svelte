66fd11f344910dab5aa900b90c1d4674fd2bc6b1
Liu <Shuiyuan>
2022-10-23 10:58:49 +0800

    add: sevelte-demo

--- svelte/svelte-demo/src/App.svelte
+++ svelte/svelte-demo/src/App.svelte
@@ -1,21 +1,10 @@
 <script>
-	let name = 'World';
-	let count = 0
-
-	function handleClick() {
-		count += 1;
-	}
-	function handleChane(e) {
-		name = e.targer.value;
-	}
+	export let name;
 </script>
 
 <main>
-	<!-- <h1>Hello {name}!</h1> -->
-	<input bind:value={name} on:change={handleChane}/>
-	<!-- <button on:click={handleClick}>点击</button> -->
-	<div>{count}</div>
-	<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
